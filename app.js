9de566950a7327244a38da9df87b4a27725b1e81
Madhusudhan <madhusudhan@peoplelinkvc.com>
2016-05-06 16:30:12 +0530

    Connected through DDP

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ var vm = require('vm');
 var myContext = {
 		require: function (module) {
 			// return require(module);
-			var DDPClient = require("ddp");
+			var DDPClient = require("ddp");			 
 			var ddpclient = new DDPClient({
 			  host : "localhost",
 			  port : 3000,
 			  ssl  : false,
 			  autoReconnect : true,
 			  autoReconnectTimer : 500,
-			  maintainCollections : true,
-			  ddpVersion : '1',  // ['1', 'pre2', 'pre1'] available
+			  maintainCollections : false,
+			  ddpVersion : '1',
 			  useSockJs: false
-			  // url: 'wss://example.com/websocket'
 			});
-
+			 
 			ddpclient.connect(function(error, wasReconnect) {
 			  if (error) {
 			    console.log('DDP connection error!');
@@ -26,19 +25,66 @@ var myContext = {
 			    console.log('Reestablishment of a connection.');
 			  }
 			  console.log('connected!');
+			  setTimeout(function () {
+			    ddpclient.call('callIt', function (err, result) { 
+			      	console.log('called function, result: ' + result);
+			    	}, function () {
+			        console.log('updated'); 
+			    	});
+			  }, 3000);
 			});
-
-			ddpclient.call( 'callIt', ['foo', 'bar'], function (err, result) {
-			  console.log('called function, result: ' + err);
-			});
-
-			setTimeout(function () {
-				ddpclient.close();
-			}, 2000);
-	
 		}
 }
 
 // vm.runInNewContext("http = require('http'); var util = require('util'); var options = { host: 'www.google.co.in', path:'/?gfe_rd=cr&amp;ei=7lMsV6alNs-L8QeP-YeABg' }; callback = function(response) { var str = '';response.on('data', function (chunk) { str += chunk; }); response.on('end', function () {util.log(str); }); }; http.request(options, callback).end();", myContext);	
 
 vm.runInNewContext("require('http')", myContext);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
